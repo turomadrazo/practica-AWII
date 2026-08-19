8af14353245f158753d8fd740599fb7aeb068781
turomadrazo <turomadrazo@gmail.com>
2026-08-18 21:46:51 -0600

    Actualización proyecto 18 Agosto

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Producto, Pedido
 
+admin.site.site_header = "Admon Cafeturo"
+admin.site.site_title = "Panel Cafeturo"
+admin.site.index_title = "Bienvenido al Panel de Administración de Cafeturo"
+
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'precio', 'categoria', 'disponible')
